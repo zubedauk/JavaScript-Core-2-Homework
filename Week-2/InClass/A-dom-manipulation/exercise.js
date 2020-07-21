767b5442063110b525edf830a39ec9d885ad309b
zubeda <zubedauk@gmail.com>
2020-07-21 06:11:46 +0100

    InClass

--- Week-2/InClass/A-dom-manipulation/exercise.js
+++ Week-2/InClass/A-dom-manipulation/exercise.js
@@ -15,30 +15,52 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
-
+let para=document.querySelectorAll("p");
+console.log(para.length);
+for(let p of para){
+    console.log("paragraph="+p);
+    console.log("paragraph text="+p.textContent);
+    console.log("paragraph html="+p.innerHTML);
+}
 
 /*
 Task 2
 ======
 
-When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
-*/
-
-
+When a user clicks the 'ALERT' button, an alert box should pop up with the text 
+"Thanks for visiting Bikes for Refugees!"
+alertBtn*/
+let alertBtn=document.querySelector("#alertBtn");
+alertBtn.addEventListener("click",function(){
+    alert("Thanks for visiting Bikes for Refugees!");
+})
 /*
 Task 3
 =======
 
-Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
-*/
-
+Write JavaScript below that changes the background colour of the page 
+when the 'Change colour' button is clicked.
+bgrChangeBtn*/
+let bgrChangeBtn=document.querySelector("#bgrChangeBtn");
+let body=document.querySelector("body");
+let color=["red","green","blue","yellow"]
+bgrChangeBtn.addEventListener("click",function(){
+  body.style.background=color[Math.floor(Math.random()*color.length)];
+})
 
 /*
 Task 4
 ======
 
-When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
-*/
+When a user clicks the 'Add some text' button, a new paragraph
+ should be added below the buttons that says "Read more below."
+AddTextBtn*/
+let AddTextBtn=document.querySelector("#AddTextBtn");
+let newPara=document.createElement("p");
+newPara.textContent="Read more below";
+AddTextBtn.addEventListener("click",function(){
+    AddTextBtn.appendChild(newPara);   
+})
 
 
 
@@ -46,5 +68,13 @@ When a user clicks the 'Add some text' button, a new paragraph should be added b
 Task 5
 ======
 
-When the 'Larger links!' button is clicked, the text of all links on the page should increase.
-*/
+When the 'Larger links!' button is clicked,
+ the text of all links on the page should increase.
+largerLinksBtn*/
+let largerLinksBtn=document.querySelector("#largerLinksBtn");
+let links=document.querySelectorAll("a");
+largerLinksBtn.addEventListener("click",function(){
+    for(link of links){
+        link.style.fontSize="xx-Large";
+    }
+})
